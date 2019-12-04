9a0e2c7f0c528f8dcdf6c92f9dcbeeb15d93dd84
garuna-m6 <anu01.m@gmail.com>
2019-12-05 00:01:35 +0530

    leaving so :(

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import os
 import sys
-class Game(object):
+class Board(object):
 	def __init__(self,size):
 		self.size = size
 		self.board1 = np.chararray([self.size,self.size])
@@ -16,13 +16,14 @@ class Game(object):
 		
 
 class Player():
-	def __init__(self,missiles,M,ships):
+	def __init__(self,missiles,boardsize,ships):
 		self.missiles = missiles
-		self.M = M
+		self.boardsize = boardsize
 		self.ships = ships
 		# self.board = 
 	def CheckValid(x,y):
 		if (x<1 or x > self.M or y<1 or y > self.M):
+			print("Wrong input")
 			return False
 		return True
 	def InitialzeBoard(board):
@@ -31,22 +32,52 @@ class Player():
 			y = int(raw_input("y"))
 			if self.CheckValid(x,y):
 				board[x,y]  = "B"
+	def MakeMove(board,x,y):
+		x = int(raw_input("x"))
+		y = int(raw_input("y"))
+		if self.CheckValid(x,y):
+			if(board[x,y] == "B"):
+				board[x,y]  = "X"
+			else:
+				board[x,y] = "0"
+
 
 	
+def FileReader(filename,board1,board2):
+	file = open(filename,"r")
+	lineidx = 0
+	result  = []
+	for line in file:
+		elif(lineidx == 0):
+			res.append(line)
+		elif(lineidx == 1):
+			line.split(":")
+			
+		elif(lineidx == 2):
+			input1
+		elif(lineidx == 3):
+			input1
+		elif(lineidx == 4):
+			input1
+		elif(lineidx == 5):
+			input1
+		else :
+			pass
+		lineidx++
 
 
 def main():
-	# board1 = np.array[]
 	boardsize = int(input("Enter size of Board "))
-	game = Game(boardsize)
-	# print("oof")
-	game.PrintBoard()
-	# player1 = Player()
+	board = Board(boardsize)
+	board.PrintBoard()
+	FileReader("input.txt",board.board1,board.board2)	
+	# player1 = Player(boardsize,boardsize,boardsize)
+	# player2 = Player(boardsize,boardsize,boardsize)
+
 	# player2 = Player()
 
 
-	# game.InitialzeGame()
-	game.PrintBoard()
+	board.PrintBoard()
 
 main()
 
